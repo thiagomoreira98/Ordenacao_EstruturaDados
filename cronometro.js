ee417b413e20a10482f73ef6b369492f778a23b8
Thiago Moreira <moreira.g.thiago@gmail.com>
2017-06-09 13:43:40 -0300

    adicionando arquivo principal do trabalho

--- cronometro.js
+++ cronometro.js
@@ -9,15 +9,15 @@ function Cronometro(nome) {
   this.fim=0;
 
   this.inicio = function() {
-    this.inicio = new Date();
+    this.inicio = performance.now();
   }
 
   this.fim = function() {
-    this.fim = new Date();
+    this.fim = performance.now();
   }
 
   this.total = function() {
-    return this.fim.converter()-this.inicio.converter();
+    return this.fim()-this.inicio();
   }
 
   this.resumo=function() {
@@ -26,7 +26,7 @@ function Cronometro(nome) {
     console.log('\nInicio:\t'+ this.inicio + '\n' );
     console.log('Fim:\t'   + this.fim  + '\n'    );
 
-    console.log( "\t" + this.fim.converter() +" ms - "+ this.inicio.converter() + " ms\n");
+    console.log( "\t" + this.fim +" ms - "+ this.inicio + " ms\n");
     console.log('Total:\t' + this.total() +" ms\n");
     console.log("-------------  -------------\n");
   }
